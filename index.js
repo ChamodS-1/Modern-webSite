d453f49927ab209f10fbaa92a8029ec2999e79d7
chamod Basnayake <chamoddousl@gmail.com>
2023-11-27 22:35:51 +0530

    modern new

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const button = document.getElementById('clickButton');
-button.addEventListener('click', ()=> {
-    console.log('hello');
-})
+const dropDown = document.getElementById("dropDownList");
+const allCourses = document.querySelectorAll(".allCourses");
+const mainCourseDiv = document.querySelector(".courseHolder");
 
+dropDown.addEventListener("input", (e) => {
+  if (+e.target.value !== 0) {
+    mainCourseDiv.innerHTML = "";
 
-
+    allCourses.forEach((course) => {
+      if (e.target.value === course.dataset.example) {
+        mainCourseDiv.append(course);
+      }
+    });
+  } else {
+    allCourses.forEach((course) => {
+      mainCourseDiv.append(course);
+    });
+  }
+});
---
+++ index2.js
@@ -0,0 +1,42 @@
+const inputText  = document.getElementById('inputText');
+const courseTopicsNames = document.querySelectorAll('.courseTopicName');
+
+inputText.addEventListener('input', e => {
+
+    let input = e.target.value.trim();
+
+    if(input !==""){
+        dropDown.disabled = true;
+    }else{
+        dropDown.disabled = false;
+    }
+    
+    mainCourseDiv.style.display = 'grid';
+    mainCourseDiv.innerHTML = "";
+
+    courseTopicsNames.forEach( course => {
+        let splitedArray = course.textContent.trim().toLowerCase().split(' ');
+        let datavalue = course.dataset.number;
+        const wordsStartingWith = splitedArray.filter(word =>
+            word.toLowerCase().startsWith(input.toLowerCase())
+          );
+
+          console.log(wordsStartingWith);
+          if(wordsStartingWith.length!=0){
+            allCourses.forEach(mainCourse => {
+                if(datavalue===mainCourse.dataset.number){
+                    mainCourseDiv.append(mainCourse);
+                }
+            })
+            console.log(datavalue);
+          }     
+    })
+
+    if(mainCourseDiv.innerHTML === ""){
+        mainCourseDiv.style.display = 'block';
+        mainCourseDiv.innerHTML=`<h1 id="errorMessage" class="text-3xl font-medium tracking-tight text-white sm:text-4xl">No Result Found for <span class="bg-gradient-to-r from-[#53a2fe] to-[#a57af8] inline-block text-transparent bg-clip-text">"${input}" </span> Word</h1>`
+        console.log('empty');
+  }
+
+})
+
